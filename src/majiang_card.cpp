e94ae48389eb312c89487340760c202b95c268fe
WillhelmKai <Willhelmkai@outlook.com>
2021-02-20 14:06:06 +0800

    first class added

---
+++ src/majiang_card.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+
+class majiang_card
+{
+private:
+	string hua_se;
+	string shu_zi;
+
+
+
+
+public:
+	majiang_card();
+	~majiang_card();
+	
+};
+
+
+int main ()
+{
+
+}
